990e492aa2ad8cd18c3e95a53266b7281192c422
Rohit Kumar <rohitkumarthegreat1@gmail.com>
2021-11-21 14:21:20 +0530

    endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 url = 'https://rasa-server-rohit9650547345.cloud.okteto.net/webhooks/rest/webhook' ##change rasablog with your app name
 myobj = {
-"message": "hi",
+"message": "haryana",
 "sender": "Ashish",
 }
 x = requests.post(url, json = myobj)
